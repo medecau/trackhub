7cf1bd719e844270981b9d6e71060bb94ec4cd5c
medecau <medecau@gmail.com>
2009-09-11 08:29:00 +0100

    triple single quotes :P

--- root.py
+++ root.py
@@ -14,20 +14,19 @@ class RootHandler(webapp.RequestHandler):
     if trackers_list is None:
       trackers_list = TrackersHandler.trackers_list
       
-    self.response.out.write('<html><head><title>Bittorrent tracker hub - trackhub</title><head><body>' +
-                            '<center><h1>trackhub</h1><i>beta</i>' +
-                            '<h2>https://trackhub.appspot.com/announce</h2><br />' +
-                            '<h2>Usage:</h2>Add ' +
-                            '<strong>https://trackhub.appspot.com/announce</strong>' +
-                            ' has a tracker to any .torrent file.<br />' +
-                            'trackhub will then redirect requests to an open tracker.<br />' +
-                            'Note that the use of <i>https</i> instead of <i>http</i> is optional but advised.<br /><br />' +
-                            '<h2>Trackers in the list:</h2>')
+    self.response.out.write(
+    '''<html><head><title>Bittorrent tracker hub - trackhub</title><head><body>
+<center><h1>trackhub</h1><i>beta</i>
+<h2>https://trackhub.appspot.com/announce</h2><br />
+<h2>Usage:</h2>Add <strong>https://trackhub.appspot.com/announce</strong> has a tracker to any .torrent file.<br />
+trackhub will then redirect requests to an open tracker.<br />
+Note that the use of <i>https</i> instead of <i>http</i> is optional but advised.<br /><br />
+<h2>Trackers in the list:</h2>''')
     for each_tracker in trackers_list:
       self.response.out.write(each_tracker + 'announce<br />')
-    self.response.out.write('<hr /><i>Ask not what bittorrent can do for you but what can you do for bittorrent.</i><br />' +
-                            '<a href="http://medecau.github.com/trackhub">kopimi</a> - ' +
-                            '<a href="http://appspot.com">thanks!</a></center></body></html>')
+    self.response.out.write(
+    '''<hr /><i>Ask not what bittorrent can do for you but what can you do for bittorrent.</i><br />
+<a href="http://medecau.github.com/trackhub">kopimi</a> - <a href="http://appspot.com">thanks!</a></center></body></html>''')
 
 
 def main():
