b1d26c993e6db7b8ee5d8c9fb8afeab02faa75a1
medecau <medecau@gmail.com>
2009-09-15 10:10:11 +0100

    experimenting with one less if:

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ tHandler = TrackersHandler()
 
 class AnnounceHandler(webapp.RequestHandler):
   def get(self):
-    if self.request.get('info_hash') is None:
-      self.response.out.write('Invalid Request: yes its working but you need to RTFM')
     try:
       self.response.headers.add_header("Expires", str(time.asctime( time.gmtime(time.time()+86400))) + " GMT")
       self.redirect(tHandler.pick_tracker(self) + '?' + self.request.query_string, permanent=True)
     except:
-      self.response.out.write('d14:failure reason31:No trackers available, sorry :(e')
-      logging.warning('trackers_list was empty - ' + str(sys.exc_info()[0]))
+      if self.request.get('info_hash') is None:
+        self.response.out.write('Invalid Request: yes its working but you need to RTFM')
+      else:
+        self.response.out.write('d14:failure reason31:No trackers available, sorry :(e')
+        logging.warning('trackers_list was empty - ' + str(sys.exc_info()[0]))
 
 class ScrapeHandler(webapp.RequestHandler):
   def get(self):
