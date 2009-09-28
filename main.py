c796b1731583554eda9739a01ef46224fec09b4f
medecau <medecau@gmail.com>
2009-09-28 13:53:41 +0100

    lets loose some wheight

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #Kopimi -- No license.
 
-import wsgiref.handlers
 import re
-from google.appengine.ext import webapp
+from os import environ
 from google.appengine.api import memcache
 from trackers_handler import TrackersHandler
 
@@ -17,13 +16,13 @@ redirect_cache={}
 info_hash_pattern=re.compile(r".*info_hash=([^?]+).*")
 
 
-def pick_tracker (self, scrape=False): #CHOOSE ONE TRACKER
+def pick_tracker (query_string, scrape=False): #CHOOSE ONE TRACKER
   import time
   global tHandler, cache_max_age, trackers_list, cache_reset_time, redirect_cache, info_hash_pattern
     
   # GET THE INT VALUE OF THE FIRST BYTE FROM THE HASH INFO 
   import urllib
-  urlencoded_info_hash=info_hash_pattern.match(self.request.query_string).group(1)
+  urlencoded_info_hash=info_hash_pattern.match(query_string).group(1)
   first_char = ord(urllib.unquote(urlencoded_info_hash)[:1])
     
   try: # TRY TO GET THE PICK FROM LOCAL MEMORY CACHE
@@ -47,39 +46,17 @@ def pick_tracker (self, scrape=False): #CHOOSE ONE TRACKER
     return tracker
     
 def redir(url):
-  print 'HTTP/1.1 301 Moved Permanently\nLocation: '+url+'\n\n'
-
-class AnnounceHandler(webapp.RequestHandler):
-  def get(self):
-    try:
-      redir(pick_tracker(self) + '?' + self.request.query_string)
-    except:
-      if self.request.get('info_hash') is None:
-        self.response.out.write('Invalid Request: yes its working but you need to RTFM')
-      else:
-        self.response.out.write('d14:failure reason31:No trackers available, sorry :(e')
-        import logging
-        import sys
-        logging.warning('trackers_list was empty - ' + str(sys.exc_info()[0]))
-        raise
-
-class ScrapeHandler(webapp.RequestHandler):
-  def get(self):
-    try:
-      redir(pick_tracker(self, True) + '?' + self.request.query_string)
-    except:
-      if self.request.get('info_hash') is None:
-        self.response.out.write('Invalid Request: yes its working but you need to RTFM')
-      else:
-        self.response.out.write('d14:failure reason31:No trackers available, sorry :(e')
-        import logging
-        logging.warning('trackers_list was empty')
+  print 'Status: 301 Moved Permanently'
+  print 'Location: '+url
+  print ''
+  return
 
 def main():
-  application = webapp.WSGIApplication([('/announce', AnnounceHandler),
-                                        ('/scrape', ScrapeHandler)])
-  wsgiref.handlers.CGIHandler().run(application)
-
+  if environ['PATH_INFO'] == '/scrape':
+    tracker = pick_tracker(environ['QUERY_STRING'],True)
+  else:
+    tracker = pick_tracker(environ['QUERY_STRING'])
+  redir(pick_tracker(environ['QUERY_STRING']) + '?' + environ['QUERY_STRING'])
 
 if __name__ == '__main__':
   main()
