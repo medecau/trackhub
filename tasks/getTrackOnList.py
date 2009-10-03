4c1d8fdffa09b9200ee26a0dec3936fb8d946aa8
medecau <medecau@gmail.com>
2009-10-03 15:12:34 +0100

    track-on list

---
+++ tasks/getTrackOnList.py
@@ -0,0 +1,14 @@
+#Kopimi -- No license.
+
+from google.appengine.api import urlfetch
+from google.appengine.api import memcache
+
+api_url='http://track-on.appspot.com/api/live'
+
+result = urlfetch.fetch(api_url)
+if result.status_code==200:
+  trackers=result.content.split('\n')
+  while trackers.count('')>0:
+    trackers.remove('')
+  trackers.sort()
+  memcache.set('trackers_list', trackers)
