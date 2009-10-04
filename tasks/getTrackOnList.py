6c7f35eaf614b0c950b4b4a5dbf36c1314c4e8c6
medecau <medecau@gmail.com>
2009-10-04 10:19:21 +0100

    while instead of if and some comments

--- tasks/getTrackOnList.py
+++ tasks/getTrackOnList.py
@@ -3,21 +3,24 @@
 from google.appengine.api import urlfetch
 from google.appengine.api import memcache
 
+self_domain='trackhub.appspot.com'
 api_url='http://track-on.appspot.com/api/live'
 
 result = urlfetch.fetch(api_url)
 if result.status_code==200:
-  trackers=result.content.split('\n')
-  while trackers.count('')>0:
-    trackers.remove('')
-  if trackers.count('http://trackhub.appspot.com/announce')>0:
-    trackers.remove('http://trackhub.appspot.com/announce')
-  if trackers.count('https://trackhub.appspot.com/announce')>0:
-    trackers.remove('https://trackhub.appspot.com/announce')
-  for tracker in trackers:
-    if tracker[:5] == 'https':
-      if trackers.count(tracker.replace('https','http'))>0:
-        trackers.remove(tracker.replace('https','http'))
+  trackers=result.content.split('\n') # FUCK XML!
+  while trackers.count('')>0:  #
+    trackers.remove('')        # REMOVE EMPTY LINES FROM THE LIST
+    
+  while trackers.count('http://'+self_domain+'/announce')>0:   #
+    trackers.remove('http://'+self_domain+'/announce')         #
+  while trackers.count('https://'+self_domain+'/announce')>0:  #
+    trackers.remove('https://'+self_domain+'/announce')        # REMOVE SELF FROM THE LIST TO AVOID REDIRECT LOOP
+    
+  for tracker in trackers:                                     #
+    if tracker[:5] == 'https':                                 #
+      while trackers.count(tracker.replace('https','http'))>0: #
+        trackers.remove(tracker.replace('https','http'))       # REMOVE DUAL ENTRANCE TRACKERS
           
   trackers.sort()
   memcache.set('trackers_list', trackers)
