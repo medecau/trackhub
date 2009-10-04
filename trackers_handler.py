b652b8edddac10766aadabe27a1c55ae9a47accc
medecau <medecau@gmail.com>
2009-10-04 09:14:53 +0100

    updated default list

--- trackers_handler.py
+++ trackers_handler.py
@@ -3,10 +3,11 @@
 from google.appengine.api import memcache
 
 class TrackersHandler():
-  trackers_list = ['http://tracker.openbittorrent.com/announce',
-                   'http://tracker.publicbt.com/announce',
+  trackers_list = ['http://bittrk.appspot.com/announce',
                    'http://nemesis.1337x.org/announce',
+                   'http://torrent.ipnm.ru/announce',
                    'http://tracker.bittorrent.am/announce',
-                   'http://tracker.publicbits.com/announce',
-                   'http://tracko.appspot.com/announce',
-                   'https://bittrk.appspot.com/announce']
+                   'http://tracker.openbittorrent.com/announce',
+                   'http://tracker.publicbt.com/announce',
+                   'https://bittrk.appspot.com/announce',
+                   'https://memtracker.appspot.com/announce'].sort()
